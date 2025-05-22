6750acedfb71eade4efe8ece0b38621c78115ea0
Zach <87861134+Kansas-Guy@users.noreply.github.com>
2025-05-22 17:01:25 -0500

    make Blackbaud Checkout work collects card information correctly

--- donations/urls.py
+++ donations/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-path('', views.donate_form, name='donate_form'),
-path('finalize', views.donate_finalize, name='donate_finalize'),
-path('webhooks/bbms', views.bbms_webhook, name='bbms_webhook'),
+    path('', views.donate_form, name='donate_form'),
+    path('finalize', views.donate_finalize, name='donate_finalize'),
+    path('webhooks/bbms', views.bbms_webhook, name='bbms_webhook'),
+    path('test-email/', views.test_send_email, name='test_send_email'),
+
 ]
